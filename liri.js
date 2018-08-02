7174bc139218e45652a70d7cfd9b16d96bcf9974
Paul Park <park.paul.94@gmail.com>
2018-08-02 08:58:06 -0400

    liri

---
+++ liri.js
@@ -0,0 +1,214 @@
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
+var keys = require('./keys.js');
+var twitterKeys = keys.twitterKeys;
+
+var args = process.argv;
+var liriCommand = args[2];
+var liriArg = '';
+
+for (var i = 3; i < args.length; i++) {
+	liriArg += args[i] + ' ';
+}
+
+function retrieveTweets() {
+	fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
+		if (err) throw err;
+	});
+	var client = new Twitter(twitterKeys);
+	var params = {screen_name: 'MyTurtleDuck', count: 20};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			var errorStr = 'ERROR: Retrieving user tweets -- ' + error;
+			fs.appendFile('./log.txt', errorStr, (err) => {
+				if (err) throw err;
+				console.log(errorStr);
+			});
+			return;
+		} else {
+			var outputStr = '------------------------\n' +
+							'User Tweets:\n' + 
+							'------------------------\n\n';
+
+			for (var i = 0; i < tweets.length; i++) {
+				outputStr += 'Created on: ' + tweets[i].created_at + '\n' + 
+							 'Tweet content: ' + tweets[i].text + '\n' +
+							 '------------------------\n';
+			}
+			fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+				if (err) throw err;
+				console.log(outputStr);
+			});
+		}
+	});
+}
+
+function spotifySong(song) {
+	fs.appendFile('./log.txt', 'User Command: node liri.js spotify-this-song ' + song + '\n\n', (err) => {
+		if (err) throw err;
+    });
+    
+	var search;
+	if (song === '') {
+		search = 'Hello';
+	} else {
+		search = song;
+	}
+
+	spotify.search({ type: 'track', query: search}, function(error, data) {
+	    if (error) {
+			var errorStr1 = 'ERROR: Retrieving Spotify track -- ' + error;
+
+			// Append the error string to the log file
+			fs.appendFile('./log.txt', errorStr1, (err) => {
+				if (err) throw err;
+				console.log(errorStr1);
+			});
+			return;
+	    } else {
+            console.log(data);
+			var songInfo = data.tracks.items[0];
+			if (!songInfo) {
+				var errorStr2 = 'ERROR: No song info retrieved, please check the spelling!';
+
+				fs.appendFile('./log.txt', errorStr2, (err) => {
+					if (err) throw err;
+					console.log(errorStr2);
+				});
+				return;
+			} else {
+				var outputStr = '------------------------\n' + 
+								'Song Information:\n' + 
+								'------------------------\n\n' + 
+								'Song Name: ' + songInfo.name + '\n'+ 
+								'Artist: ' + songInfo.artists[0].name + '\n' + 
+								'Album: ' + songInfo.album.name + '\n' + 
+								'Preview Here: ' + songInfo.preview_url + '\n';
+
+				fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+					if (err) throw err;
+					console.log(outputStr);
+				});
+			}
+	    }
+	});
+}
+
+function retrieveOBDBInfo(movie) {
+	fs.appendFile('./log.txt', 'User Command: node liri.js movie-this ' + movie + '\n\n', (err) => {
+		if (err) throw err;
+	});
+	var search;
+	if (movie === '') {
+		search = 'Iron Man';
+	} else {
+		search = movie;
+    }
+    
+	search = search.split(' ').join('+');
+
+	var queryStr = 'http://www.omdbapi.com/?t=' + search + '&tomatoes=true&apikey=70727a7d';
+
+	request(queryStr, function (error, response, body) {
+		if ( error || (response.statusCode !== 200) ) {
+			var errorStr1 = 'ERROR: Retrieving OMDB entry -- ' + error;
+
+			fs.appendFile('./log.txt', errorStr1, (err) => {
+				if (err) throw err;
+				console.log(errorStr1);
+			});
+			return;
+		} else {
+			var data = JSON.parse(body);
+			if (!data.Title && !data.Released && !data.imdbRating) {
+				var errorStr2 = 'ERROR: No movie info retrieved, please check the spelling of the movie name!';
+
+				fs.appendFile('./log.txt', errorStr2, (err) => {
+					if (err) throw err;
+					console.log(errorStr2);
+				});
+				return;
+			} else {
+		    	var outputStr = '------------------------\n' + 
+								data.Title + ' Information:\n' + 
+								'------------------------\n\n' +
+								'Movie Title: ' + data.Title + '\n' + 
+								'Year Released: ' + data.Released + '\n' +
+								'IMBD Rating: ' + data.imdbRating + '\n' +
+								'Rotten Tomatoes Rating: ' + data.tomatoRating + '\n' +
+								'Country Produced: ' + data.Country + '\n' +
+								'Language: ' + data.Language + '\n' +
+								'Actors: ' + data.Actors + '\n';
+
+				fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+					if (err) throw err;
+					console.log(outputStr);
+				});
+			}
+		}
+	});
+}
+
+function doAsYerTold() {
+	fs.appendFile('./log.txt', 'User Command: node liri.js do-what-it-says\n\n', (err) => {
+		if (err) throw err;
+	});
+
+	fs.readFile('./log.txt', 'utf8', function (error, data) {
+		if (error) {
+			console.log('ERROR: Reading log.txt -- ' + error);
+			return;
+		} else {
+			var cmdString = data.split(',');
+			var command = cmdString[0].trim();
+			var param = cmdString[1].trim();
+
+			switch(command) {
+				case 'my-tweets':
+					retrieveTweets(); 
+					break;
+
+				case 'spotify-this-song':
+					spotifySong(param);
+					break;
+
+				case 'movie-this':
+					retrieveOBDBInfo(param);
+					break;
+			}
+		}
+	});
+}
+
+if (liriCommand === 'my-tweets') {
+	retrieveTweets(); 
+
+} else if (liriCommand === `spotify-this-song`) {
+	spotifySong(liriArg);
+
+} else if (liriCommand === `movie-this`) {
+	retrieveOBDBInfo(liriArg);
+
+} else if (liriCommand ===  `do-what-it-says`) {
+	doAsYerTold();
+
+} else {
+	fs.appendFile('./log.txt', 'User Command: ' + args + '\n\n', (err) => {
+		if (err) throw err;
+
+		outputStr = 'Usage:\n' + 
+				   '    node liri.js my-tweets\n' + 
+				   '    node liri.js spotify-this-song "<song_name>"\n' + 
+				   '    node liri.js movie-this "<movie_name>"\n' + 
+				   '    node liri.js do-what-it-says\n';
+
+		fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+			if (err) throw err;
+			console.log(outputStr);
+		});
+	});
+}
